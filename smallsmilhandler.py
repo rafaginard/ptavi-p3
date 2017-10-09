51d3e4343426cbcf9d97f568eaa4f96d83880082
Rafael Ginard Aguilera <rginard@gamma05.aulas.gsyc.urjc.es>
2017-10-09 14:31:39 +0200

    Modificado imprimir

--- smallsmilhandler.py
+++ smallsmilhandler.py
@@ -3,54 +3,56 @@
 
 from xml.sax import make_parser
 from xml.sax.handler import ContentHandler
+import collections
 
 class SmallSMILHandler(ContentHandler):
 
     def __init__(self):
         self.etiquetas = []
 
-
     def startElement(self, name, attrs):
-        atributos = {}
         if name == "root-layout":
-            root_layout = {}
+            root_layout = collections.OrderedDict()
             root_layout["element"] = "root-layout"
-            root_layout["width"] = attrs.get("width", "")
-            root_layout["height"] = attrs.get("height", "")
-            root_layout["background-color"] = attrs.get("background-color", "")
+            root_layout["width"] = attrs.get("width", "empty")
+            root_layout["height"] = attrs.get("height", "empty")
+            root_layout["background-color"] = attrs.get("background-color",
+                                                        "empty")
             self.etiquetas.append(root_layout)
         elif name == "region":
-            region = {}
+            region = collections.OrderedDict()
             region["element"] = "region"
-            region["id"] = attrs.get("id", "")
-            region["top"] = attrs.get("top", "")
-            region["bottom"] = attrs.get("bottom", "")
-            region["left"] = attrs.get("left", "")
-            region["right"] = attrs.get("right", "")
+            region["id"] = attrs.get("id", "empty")
+            region["top"] = attrs.get("top", "empty")
+            region["bottom"] = attrs.get("bottom", "empty")
+            region["left"] = attrs.get("left", "empty")
+            region["right"] = attrs.get("right", "empty")
             self.etiquetas.append(region)
         elif name == "img":
-            img = {}
+            img = collections.OrderedDict()
             img["element"] = "img"
-            img["src"] = attrs.get("src", "")
-            img["region"] = attrs.get("region", "")
-            img["begin"] = attrs.get("begin", "")
-            img["dur"] = attrs.get("dur", "")
+            img["src"] = attrs.get("src", "empty")
+            img["region"] = attrs.get("region", "empty")
+            img["begin"] = attrs.get("begin", "empty")
+            img["dur"] = attrs.get("dur", "empty")
             self.etiquetas.append(img)
         elif name == "audio":
-            audio = {}
+            audio = collections.OrderedDict()
             audio["element"] = "audio"
-            audio["src"] = attrs.get("src", "")
-            audio["begin"] = attrs.get("begin", "")
-            audio["dur"] = attrs.get("dur", "")
+            audio["src"] = attrs.get("src", "empty")
+            audio["begin"] = attrs.get("begin", "empty")
+            audio["dur"] = attrs.get("dur", "empty")
             self.etiquetas.append(audio)
         elif name == "textstream":
-            textstream = {}
+            textstream = collections.OrderedDict()
             textstream["element"] = "textstream"
-            textstream["src"] = attrs.get("src", "")
-            textstream["region"] = attrs.get("region", "")
+            textstream["src"] = attrs.get("src", "empty")
+            textstream["region"] = attrs.get("region", "empty")
             self.etiquetas.append(textstream)
     def get_tags(self):
-            print(self.etiquetas)
+            for etiqueta in self.etiquetas:
+                for clave, atributo in etiqueta.items():
+                    print(clave + "---> " + atributo)
 if __name__ == "__main__":
     parser = make_parser()
     sHandler = SmallSMILHandler()
