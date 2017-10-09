0b25aa43d1e83a68c0f0286f38ee1ccd36cafb63
Rafael Ginard Aguilera <rginard@gamma05.aulas.gsyc.urjc.es>
2017-10-09 16:44:05 +0200

    Karaoke con json

--- karaoke.py
+++ karaoke.py
@@ -3,28 +3,31 @@
 
 from xml.sax import make_parser
 from xml.sax.handler import ContentHandler
+from  urllib.request import urlretrieve
+import json
 import sys
 import smallsmilhandler
+
 
 if __name__ == "__main__":
     parser = make_parser()
     sHandler = smallsmilhandler.SmallSMILHandler()
     parser.setContentHandler(sHandler)
+    url = ""
     try:
         parser.parse(open(sys.argv[1]))
     except IndexError:
         sys.exit("Usage: python3 karaoke.py file.smil")
 
-
-#    for etiqueta in self.etiquetas:
-#        for clave, atributo in etiqueta.items():
-#            print(clave + "---> " + atributo)
-
-
-    for etiquetas in sHandler.etiquetas:
+    for etiquetas in sHandler.lista_etiquetas:
         lista_atributos = []
-        lista = ""
+
         for clave, atributo in etiquetas.items():
             if clave != "element":
                 lista_atributos += ("\t", clave," = ", atributo, " ")
-        print(etiquetas["element"], lista.join(lista_atributos))
+            if clave == "src" and not atributo.find("http://"):
+                print("HASTA AQUI: " + atributo)
+                urlretrieve(atributo, "local_file.rt")
+        print(etiquetas["element"], "".join(lista_atributos))
+
+    json.dump([sHandler.lista_etiquetas], open("karaoke.json", "w"))
