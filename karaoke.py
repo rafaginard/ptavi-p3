2af6b73a79a71baf3cf51d85ec5a998c7f45ac8f
Rafael Ginard Aguilera <rginard@gamma05.aulas.gsyc.urjc.es>
2017-10-09 14:31:22 +0200

    Karaoke me imprime cosas

--- karaoke.py
+++ karaoke.py
@@ -1,7 +1,30 @@
 #!/usr/bin/python3
 # -*- coding: utf-8 -*-
 
+from xml.sax import make_parser
+from xml.sax.handler import ContentHandler
+import sys
 import smallsmilhandler
 
-if __name__ == "__main__"
-    
+if __name__ == "__main__":
+    parser = make_parser()
+    sHandler = smallsmilhandler.SmallSMILHandler()
+    parser.setContentHandler(sHandler)
+    try:
+        parser.parse(open(sys.argv[1]))
+    except IndexError:
+        sys.exit("Usage: python3 karaoke.py file.smil")
+
+
+#    for etiqueta in self.etiquetas:
+#        for clave, atributo in etiqueta.items():
+#            print(clave + "---> " + atributo)
+
+
+    for etiquetas in sHandler.etiquetas:
+        lista_atributos = []
+        lista = ""
+        for clave, atributo in etiquetas.items():
+            if clave != "element":
+                lista_atributos += ("\t", clave," = ", atributo, " ")
+        print(etiquetas["element"], lista.join(lista_atributos))
