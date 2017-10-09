259c2b6fd4ba73a0dd9a244cc080e4133bdd4e68
Rafael Ginard Aguilera <rginard@gamma05.aulas.gsyc.urjc.es>
2017-10-09 10:59:26 +0200

    Primeros pasos

--- smallsmilhandler.py
+++ smallsmilhandler.py
@@ -1,4 +1,28 @@
 #!/usr/bin/python3
 # -*- coding: utf-8 -*-
 
+from xml.sax import make_parser
+from xml.sax.handler import ContentHandler
 
+class SmallSMILHandler(ContentHandler):
+
+    def __init__(self):
+        self.etiquetas = []
+
+
+    def startElement(self, name, attrs):
+        atributos = {}
+        if name == "root-layout":
+            self.etiquetas = ["root-layout"]                                                         ]
+            atributos["width"] = attrs.get("width", "")
+            atributos["height"] = attrs.get("height", "")
+            atributos["background-color"] = attrs.get("background-color", "")
+
+            print(self.etiquetas[0])
+
+
+if __name__ == "__main__":
+    parser = make_parser()
+    sHandler = SmallSMILHandler()
+    parser.setContentHandler(sHandler)
+    parser.parse(open("karaoke.smil"))
