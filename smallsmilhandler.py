9457dd0e7a4a20d69515747fefb5e155d693d62a
Rafael Ginard Aguilera <rginard@alpha49.aulas.gsyc.urjc.es>
2017-10-11 11:54:52 +0200

    Modificado Init

--- smallsmilhandler.py
+++ smallsmilhandler.py
@@ -8,24 +8,24 @@ class SmallSMILHandler(ContentHandler):
 
     def __init__(self):
         self.lista_etiquetas = []
-
-    def startElement(self, name, attrs):
-        etiquetas = ["root-layout", "region", "img", "audio", "textstream"]
-        attrDict = {"root-layout": ["width", "height", "background-color"],
+        self.etiquetas = ["root-layout", "region", "img", "audio", "textstream"]
+        self.attrDict = {"root-layout": ["width", "height", "background-color"],
                     "region": ["src", "top", "bottom", "left", "right"],
                     "img": ["src", "region", "begin", "dur"],
                     "audio": ["src", "begin", "dur"],
                     "textstream": ["src", "region"]}
-        if name in etiquetas:
+    def startElement(self, name, attrs):
+
+        if name in self.etiquetas:
             tempDcit = {}
             tempDcit["element"] = name
-            for atribute in attrDict[name]:
+            for atribute in self.attrDict[name]:
                 tempDcit[atribute] = attrs.get(atribute, "")
             self.lista_etiquetas.append(tempDcit)
 
     def get_tags(self):
         return self.lista_etiquetas
-        
+
 if __name__ == "__main__":
     parser = make_parser()
     sHandler = SmallSMILHandler()
