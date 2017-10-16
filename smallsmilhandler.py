4ae4282231de6106cefffbbcb4a9cf5d58572e05
Rafael Ginard Aguilera <rginard@gamma08.aulas.gsyc.urjc.es>
2017-10-16 10:17:01 +0200

    Programa terminado

--- smallsmilhandler.py
+++ smallsmilhandler.py
@@ -4,16 +4,20 @@
 from xml.sax import make_parser
 from xml.sax.handler import ContentHandler
 
+
 class SmallSMILHandler(ContentHandler):
 
     def __init__(self):
         self.lista_etiquetas = []
-        self.etiquetas = ["root-layout", "region", "img", "audio", "textstream"]
-        self.attrDict = {"root-layout": ["width", "height", "background-color"],
-                    "region": ["src", "top", "bottom", "left", "right"],
-                    "img": ["src", "region", "begin", "dur"],
-                    "audio": ["src", "begin", "dur"],
-                    "textstream": ["src", "region"]}
+        self.etiquetas = ["root-layout", "region", "img", "audio",
+                          "textstream"]
+        self.attrDict = {"root-layout": ["width", "height",
+                                         "background-color"],
+                         "region": ["src", "top", "bottom", "left", "right"],
+                         "img": ["src", "region", "begin", "dur"],
+                         "audio": ["src", "begin", "dur"],
+                         "textstream": ["src", "region"]}
+
     def startElement(self, name, attrs):
 
         if name in self.etiquetas:
